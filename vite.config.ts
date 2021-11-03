4942fe6cc490a5b32e748137f6717dfc7e982c8e
Fergal Moran <fergal.moran@gmail.com>
2021-11-03 19:14:57 +0000

    Docker stuff added

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import reactRefresh from "@vitejs/plugin-react-refresh";
-import fs from "fs";
-import { defineConfig } from "vite";
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import fs from 'fs';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
     https: {
-      key: fs.readFileSync("../certs/dev.audioboos.com.key"),
-      cert: fs.readFileSync("../certs/dev.audioboos.com.crt"),
+      key: fs.readFileSync('../certs/dev.audioboos.com.key'),
+      cert: fs.readFileSync('../certs/dev.audioboos.com.crt'),
     },
   },
   plugins: [reactRefresh()],
