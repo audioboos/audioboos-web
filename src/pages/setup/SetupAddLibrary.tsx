65827a7f04d0868547352e317fda54c8d810dae6
Fergal Moran <fergal.moran@gmail.com>
2021-11-23 17:50:28 +0000

    Fix library setup flow

--- src/pages/setup/SetupAddLibrary.tsx
+++ src/pages/setup/SetupAddLibrary.tsx
@@ -1,7 +1,6 @@
 import 'font-awesome/css/font-awesome.min.css';
 import React from 'react';
 import { MdDriveFolderUpload, MdFolderOpen } from 'react-icons/md';
-import { useHistory } from 'react-router-dom';
 import IHookFormProps from '../../components/IHookFormProps';
 import fileSystemService from '../../services/api/fileSystemService';
 
@@ -9,23 +8,15 @@ interface IFileInfo {
   key: string;
 }
 const SetupAddLibrary = ({ register }: IHookFormProps) => {
-  const defaultPath = '/home/fergalm/working/audioboos-library';
+  const defaultPath = '/';
   const [files, setFiles] = React.useState<IFileInfo[]>();
   const [currentPath, setCurrentPath] = React.useState(defaultPath);
 
-  const history = useHistory();
-  const _navigateUp = () => {
+  const _navigateUp = ($event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    $event.preventDefault();
     const newPath = currentPath.substring(0, currentPath.lastIndexOf('/'));
     setCurrentPath(newPath || defaultPath);
   };
-
-  //   React.useEffect(() => {
-  //     return () => {
-  //       if (history.action === 'POP' && currentPath !== '/') {
-  //         alert('We go back');
-  //       }
-  //     };
-  //   }, [history]);
 
   React.useEffect(() => {
     (async () => {
@@ -60,7 +51,7 @@ const SetupAddLibrary = ({ register }: IHookFormProps) => {
       </div>
       <div className="mt-4 mb-2 ml-2 text-2xl text-gray-600 rounded-sm shadow-sm ">
         <button
-          onClick={_navigateUp}
+          onClick={($e) => _navigateUp($e)}
           className="transition duration-150 ease-in-out hover:text-gray-400"
         >
           <MdDriveFolderUpload />
@@ -73,7 +64,9 @@ const SetupAddLibrary = ({ register }: IHookFormProps) => {
               <div className="cursor-pointer" onClick={() => setCurrentPath(f.key)}>
                 <div className="flex items-center">
                   <MdFolderOpen />
-                  <span className="block ml-3 font-normal truncate">{f.key}</span>
+                  <span className="block ml-3 font-normal truncate">
+                    {f.key.replace(currentPath, '').replace(/^\//g, '')}
+                  </span>
                 </div>
               </div>
             </span>
