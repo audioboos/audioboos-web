19930c62a6fac0db1e8eb82baffa1c67269086f7
Fergal Moran <fergal.moran@gmail.com>
2021-12-22 01:07:42 +0000

    Add JWT refresh

--- src/services/api/apiService.ts
+++ src/services/api/apiService.ts
@@ -1,37 +1,56 @@
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosInstance } from 'axios';
 
 class ApiService {
-    private static _client: AxiosInstance | undefined = undefined;
-    private static _cancelToken: any;
-    private static _source: any;
+  private static _client: AxiosInstance | undefined = undefined;
+  private static _cancelToken: any;
+  private static _source: any;
 
-    public cancel = () => {
-        if (
-            ApiService._client !== undefined &&
-            ApiService._source !== undefined
-        ) {
-            ApiService._source().cancel();
+  public cancel = () => {
+    if (ApiService._client !== undefined && ApiService._source !== undefined) {
+      ApiService._source().cancel();
+    }
+  };
+  protected getInstance = async (): Promise<AxiosInstance> => {
+    if (!ApiService._client) {
+      ApiService._client = await this.requestClient();
+      ApiService._cancelToken = axios.CancelToken;
+      ApiService._source = axios.CancelToken.source;
+    }
+    if (ApiService._client === undefined) {
+      throw Error('Unable to resolve axios client');
+    }
+    return ApiService._client;
+  };
+  public requestClient = async () => {
+    const instance = axios.create({
+      baseURL: import.meta.env.VITE_API_URL as string,
+      maxRedirects: 0,
+      headers: { 'Content-Type': 'application/json' },
+      withCredentials: true,
+    });
+    instance.interceptors.response.use(
+      (res) => {
+        return res;
+      },
+      async (err) => {
+        const originalConfig = err.config;
+        if (originalConfig.url !== '/auth/signin' && err.response) {
+          // Access Token was expired
+          if (err.response.status === 401 && !originalConfig._retry) {
+            originalConfig._retry = true;
+
+            try {
+              const rs = await instance.post('/auth/refresh');
+              return instance(originalConfig);
+            } catch (_error) {
+              return Promise.reject(_error);
+            }
+          }
         }
-    };
-    protected getInstance = async (): Promise<AxiosInstance> => {
-        if (!ApiService._client) {
-            ApiService._client = await this.__requestClient();
-            ApiService._cancelToken = axios.CancelToken;
-            ApiService._source = axios.CancelToken.source;
-        }
-        if (ApiService._client === undefined) {
-            throw Error("Unable to resolve axios client");
-        }
-        return ApiService._client;
-    };
-    private __requestClient = async () => {
-        const instance = axios.create({
-          baseURL: import.meta.env.VITE_API_URL as string,
-          maxRedirects: 0,
-          headers: { "Content-Type": "application/json" },
-          withCredentials: true,
-        });
-        return instance;
-    };
+        return Promise.reject(err);
+      }
+    );
+    return instance;
+  };
 }
 export default ApiService;
--- src/store/redux/api.ts
+++ src/store/redux/api.ts
@@ -1,28 +1,52 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import {
+  BaseQueryFn,
+  createApi,
+  FetchArgs,
+  fetchBaseQuery,
+  FetchBaseQueryError,
+} from '@reduxjs/toolkit/query/react';
 import { Artist, Settings } from '../../models';
-import { Profile } from "../../models/Profile";
+import { Profile } from '../../models/Profile';
+import { RootState } from './store';
+const baseQuery = fetchBaseQuery({
+  baseUrl: import.meta.env.VITE_API_URL as string,
+  credentials: 'include',
+});
+
+const baseQueryWithRefresh: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError> = async (
+  args,
+  api,
+  extraOptions
+) => {
+  let result = await baseQuery(args, api, extraOptions);
+  if (result.error && result.error.status === 401) {
+    // try to get a new token
+    const refreshResult = await baseQuery('/auth/refresh', api, extraOptions);
+    if (refreshResult.meta?.response?.status === 200) {
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      //TODO: redirect to login page or something?
+      //api.dispatch(loggedOut());
+    }
+  }
+  return result;
+};
 
 const api = createApi({
-  baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_API_URL as string,
-    prepareHeaders(headers) {
-      return headers;
-    },
-    credentials: 'include',
-  }),
+  baseQuery: baseQueryWithRefresh,
   endpoints(build) {
     return {
       artists: build.query<Array<Artist>, void>({
-        query: () => '/artists',
+        query: () => ({ url: '/artists', method: 'GET' }),
       }),
       artist: build.query<Artist, string>({
-        query: (name) => `/artists/${name}`,
+        query: (name) => ({ url: `/artists/${name}`, method: 'GET' }),
       }),
       settings: build.query<Settings, void>({
-        query: () => `/settings`,
+        query: () => ({ url: `/settings`, method: 'GET' }),
       }),
       auth: build.query<Profile, void>({
-        query: () => `/auth/profile`,
+        query: () => ({ url: `/auth/profile`, method: 'GET' }),
       }),
     };
   },
