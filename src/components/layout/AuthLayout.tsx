fffb7097367236a8c6510e1eb5c121fc95e8129a
Fergal Moran <fergal.moran@gmail.com>
2022-01-10 05:21:29 +0000

    Fix up fonts

--- src/components/layout/AuthLayout.tsx
+++ src/components/layout/AuthLayout.tsx
@@ -1,23 +1,21 @@
-import React from "react";
-import Footer from "./Footer";
-import { ILayoutProps } from "./Layout";
-import { Navbar } from "./Navbar";
-import Sidebar from "./Sidebar";
+import React from 'react';
+import Footer from './Footer';
+import { ILayoutProps } from './Layout';
+import { Navbar } from './Navbar';
+import Sidebar from './Sidebar';
 
 const AuthLayout = ({ children }: ILayoutProps) => {
-    return (
-        <div>
-            <div className="flex h-screen antialiased text-gray-900 bg-gray-100 dark:bg-dark dark:text-light">
-                <Sidebar />
-                <div className="flex flex-col w-full h-full overflow-x-hidden overflow-y-auto">
-                    <Navbar />
-                    <main className="flex-grow overflow-y-scroll">
-                        {children}
-                    </main>
-                    <Footer />
-                </div>
-            </div>
+  return (
+    <div className="font-oxygen">
+      <div className="flex h-screen antialiased text-gray-900 bg-gray-100 dark:bg-dark dark:text-light">
+        <Sidebar />
+        <div className="flex flex-col w-full h-full overflow-x-hidden overflow-y-auto">
+          <Navbar />
+          <main className="flex-grow overflow-y-scroll">{children}</main>
+          <Footer />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 export default AuthLayout;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,12 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontSize: {
-      'xs': '.75rem',
-      'sm': '.875rem',
-      'tiny': '.875rem',
-      'base': '1rem',
-      'lg': '1.125rem',
-      'xl': '1.25rem',
+      xs: '.75rem',
+      sm: '.875rem',
+      tiny: '.875rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
       '2xl': '1.5rem',
       '3xl': '1.875rem',
       '4xl': '2.25rem',
@@ -21,7 +21,7 @@ module.exports = {
     },
     extend: {
       fontFamily: {
-        Sarabun: ['"Sarabun"', 'sans-serif'],
+        oxygen: ['"Oxygen"', 'sans-serif'],
       },
     },
   },
