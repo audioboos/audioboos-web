88c59438ac08d5ad0d0d82e86a5d5f68bad5f866
Fergal Moran <fergal.moran@gmail.com>
2022-01-20 18:42:55 +0000

    Footer added back

--- src/components/layout/AuthLayout.tsx
+++ src/components/layout/AuthLayout.tsx
@@ -7,14 +7,16 @@ import Sidebar from './Sidebar';
 
 const AuthLayout = ({ children }: ILayoutProps) => {
   return (
-    <>
-      <Sidebar />
-      <div className="relative md:ml-64 bg-blueGray-100">
-        <Navbar />
-        {/* <HeaderStats />  */}
-        <div className="w-full px-4 mx-auto md:px-10">{children}</div>
+    <div className="font-alice">
+      <div className="flex h-screen antialiased text-gray-900 bg-gray-100 dark:bg-dark dark:text-light">
+        <Sidebar />
+        <div className="flex flex-col w-full h-full ml-64 overflow-x-hidden overflow-y-auto">
+          <Navbar />
+          <main className="flex-grow overflow-y-scroll">{children}</main>
+          <Footer />
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 export default AuthLayout;
