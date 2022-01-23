2f2611f5a0b056c0143e9e9e72068f1e20fd8d7d
Fergal Moran <fergal.moran@gmail.com>
2022-01-23 02:01:53 +0000

    Fix up staging env

--- scripts/docker_debug.sh
+++ scripts/docker_debug.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-docker build \
-    -t fergalmoran/audioboos-web \
-    -f docker/Dockerfile \
-    --load .
+BUILD=1
+
+if [ $BUILD -eq 1 ]; then
+    docker build \
+        --build-arg RUN_MODE=staging \
+        -t fergalmoran/audioboos-web \
+        -f docker/Dockerfile \
+        --load .
+fi
 
 docker run \
     -p 8081:80 \
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,20 @@
-import { defineConfig } from "vite";
-import mkcert from "vite-plugin-mkcert";
-
+import { defineConfig } from 'vite';
+import mkcert from 'vite-plugin-mkcert';
 export default defineConfig({
   server: {
-    host: "0.0.0.0",
-    https: true
+    host: '0.0.0.0',
+    https: true,
     // https: {
     //   key: fs.readFileSync('../certs/zerossl/private.key'),
     //   cert: fs.readFileSync('../certs/zerossl/certificate.crt'),
     // },
   },
-  plugins: [mkcert({
-    hosts: ['dev.audioboos.info']
-  })],
+  plugins: [
+    mkcert({
+      hosts: ['dev.audioboos.info'],
+    }),
+  ],
   define: {
-    "process.env": {}
-  }
+    'process.env': {},
+  },
 });
