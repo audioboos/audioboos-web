1fdd096c2741dd0fb0f74de457c4bd960695bb2c
Fergal Moran <fergal.moran@gmail.com>
2022-01-23 00:16:33 +0000

    Add the greebles

--- src/pages/greebles-page.comonent.tsx
+++ src/pages/greebles-page.comonent.tsx
@@ -2,11 +2,8 @@ import React from 'react';
 import { toast } from 'react-toastify';
 import jobService from '../services/api/job.service';
 import { Images } from '../services';
-import { useClientHints } from '../services/react-client.hints';
 import { ImageCard } from '../components/cards';
 const Greebles = () => {
-  const ch = useClientHints();
-
   return (
     <div className="flex flex-row items-center justify-center min-h-screen">
       <ImageCard
